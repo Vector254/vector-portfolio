5faa065566e80036952584333af04f40b19aebcf
Vector254 <ochrist7@gmail.com>
2020-12-09 17:33:02 +0300

    add details

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,6 +8,9 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    url('about/', views.about, name='about'),
+    url('projects/', views.projects, name='projects'),
+    url('contacts/', views.contacts, name='contacts'),
     ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,3 +5,17 @@ from django.shortcuts import render
 def index(request):
     
     return render(request,'index.html')
+
+def about(request):
+    
+    return render(request,'about.html')
+
+def projects(request):
+    
+    return render(request,'projects.html')
+
+def contacts(request):
+    
+    return render(request,'contacts.html')
+
+
