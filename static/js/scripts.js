81c80bb9d9996cd11d0c0aae85b2e22fd42b30ef
Vector254 <ochrist7@gmail.com>
2020-12-09 16:32:57 +0300

    create about page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,4 @@
-function openNav() {
+/*function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -8,4 +8,4 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
-  }
+  }*/
